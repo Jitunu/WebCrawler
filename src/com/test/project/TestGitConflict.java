d46f0d8ff37e835091578abff54bdaa89995771a
jitun Sahoo <jitunu.sahoo@wipro.com>
2020-05-24 08:51:59 +0100

    Local Commit

---
+++ src/com/test/project/TestGitConflict.java
@@ -0,0 +1,11 @@
+package com.test.project;
+
+public class TestGitConflict {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "local change";
+
+	}
+
+}
